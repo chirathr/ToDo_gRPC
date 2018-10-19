8fc8076c321eaee5a3984768df547921763064e5
Chirath R <chirath.02@gmail.com>
2018-10-19 14:49:31 +0530

    Updated server.py to use functions from todo_server_lib

--- server/server.py
+++ server/server.py
@@ -1,40 +1,28 @@
-from server.todo_db import ToDoDb
-from protobuf.todo_pb2 import User, ToDo
 from protobuf import todo_pb2_grpc
+from server.todo_server_lib.main import add_user, add_todo, update_todo, get_todo_list
 
 
 class ToDoServicer(todo_pb2_grpc.TodoServiceServicer):
     """
     Methods that implement the functionality of the ToDo server
     """
-    def __init__(self):
-        pass
 
-    def add_user(self, user, context):
-        if not user.name:
-            return User()
-        db = ToDoDb()
-        return db.add_user_if_not_exist(user)
+    def AddUser(self, request, context):
+        print(request)
+        user = add_user(request)
+        print(user)
+        return user
 
-    def add_todo(self, todo, context):
-        if not todo.text and not todo.user:
-            return ToDo()
-        db = ToDoDb()
-        return db.add_todo(todo)
+    def AddToDo(self, request, context):
+        return add_todo(request)
 
-    def update_todo(self, todo, context):
-        if not todo.text and not todo.user:
-            return ToDo()
-        db = ToDoDb()
-        return db.update_todo(todo)
+    def UpdateToDo(self, request, context):
+        return update_todo(request)
 
-    def get_todo_list(self, user, context):
-        if user.id == 0:
-            return []
-        db = ToDoDb()
-        todo_list = db.get_todo_list(user)
-        for todo in todo_list:
-            yield todo
+    def GetToDoList(self, request, context):
+        todo_list = get_todo_list(request)
+        print(todo_list)
+        return todo_list
 
     def delete_todo_list(self, user, context):
         pass
