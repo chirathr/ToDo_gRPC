cabb1d0e9d21804029f5a18d99c07e21f55e212c
Chirath R <chirath.02@gmail.com>
2018-10-20 15:04:48 +0530

    Refactored server logic to lib files

--- server/server.py
+++ server/server.py
@@ -1,5 +1,5 @@
 from protobuf import todo_pb2_grpc
-from server.todo_server_lib.main import add_user, add_todo, update_todo, get_todo_list
+from server.server_util_lib.main import add_user, add_todo, update_todo, get_todo_list
 
 
 class ToDoServicer(todo_pb2_grpc.TodoServiceServicer):
@@ -8,10 +8,7 @@ class ToDoServicer(todo_pb2_grpc.TodoServiceServicer):
     """
 
     def AddUser(self, request, context):
-        print(request)
-        user = add_user(request)
-        print(user)
-        return user
+        return add_user(request)
 
     def AddToDo(self, request, context):
         return add_todo(request)
@@ -21,8 +18,9 @@ class ToDoServicer(todo_pb2_grpc.TodoServiceServicer):
 
     def GetToDoList(self, request, context):
         todo_list = get_todo_list(request)
-        print(todo_list)
-        return todo_list
+        for todo in todo_list:
+            yield todo
 
     def delete_todo_list(self, user, context):
+        # TODO: Complete delete_todo_list()
         pass
