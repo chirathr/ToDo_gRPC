1d2a57a9fc2fc9f3295a9dd1e079ab579bb4160b
Chirath R <chirath.02@gmail.com>
2018-10-20 08:57:31 +0530

    Moved all strings to a constants file

---
+++ client/client_util_lib/string_constants.py
@@ -0,0 +1,17 @@
+WELCOME_MESSAGE = 'Welcome to Todo application '
+ENTER_NAME = 'Hi, Please enter name to continue: '
+ENTER_TODO_NO = 'Enter todo number: '
+ENTER_OPTION = 'Enter your option: '
+SEPARATOR = '#-----------------------------------------------------------#'
+SEPARATOR_COMPLETED = '#---------------------- Completed --------------------------#'
+EMPTY_TODO_LIST = 'No ToDo\'s found!'
+CHOICES = ' 1 -> Add    2 -> mark as done   3 -> delete todo    4-> Exit'
+
+ERROR_FAIL = 'Error, Something happened!'
+ERROR_TODO = 'Enter a todo: '
+ERROR_FAILED_ADD_TODO = 'Failed to add todo!'
+ERROR_WRONG_CHOICE = 'Wrong choice, Try again!'
+ERROR_FAILED_TO_MARK_TODO = 'Failed to mark todo:{0} as done'
+ERROR_FAILED_DELETE_TODO = 'Failed to delete todo:{0}'
+
+
